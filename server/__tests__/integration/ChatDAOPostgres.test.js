2b1d9cccd52dce7dd919e71fc77d5af0a15e645f
Leif <leifl@kth.se>
2020-12-09 15:42:26 +0100

    now using postgres

---
+++ server/__tests__/integration/ChatDAOPostgres.test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const pgp = require('pg-promise')();
+const ChatDAO = require('../../src/integration/ChatDAO');
+
+let db = null;
+let chatDAO = null;
+const username = 'stina';
+
+beforeAll(async () => {
+  db = await connectToChatDb();
+  await clearDb();
+});
+
+beforeEach(async () => {
+  chatDAO = new ChatDAO();
+  await forSomeReasonJestDoesNotWaitForCompletionThereforeWait();
+  await chatDAO.createTables();
+  await createUser();
+});
+
+afterEach(async () => {
+  await clearDb();
+});
+
+afterAll(async () => {
+  await clearDb();
+});
+
+describe('tests for findUserByUsername', () => {
+  test('existing user', async () => {
+    const users = await chatDAO.findUserByUsername(username);
+    expect(users[0].username).toBe(username);
+  });
+});
+
+const forSomeReasonJestDoesNotWaitForCompletionThereforeWait = async () => {
+  await sleep(100);
+};
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const connectToChatDb = () => {
+  const result = require('dotenv-safe').config();
+  if (result.error) {
+    throw result.error;
+  }
+  return pgp({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  });
+};
+
+const clearDb = async () => {
+  await db.none('drop table if exists msgs');
+  await db.none('drop table if exists users');
+};
+
+const createUser = async () => {
+  const data = {
+    username: username,
+    createdAt: 'now()',
+    updatedAt: 'now()',
+  };
+  await db.none(
+    'insert into ${table:name} (${valuesToInsert:name}) values (${valuesToInsert:csv})',
+    {valuesToInsert: data, table: 'users'}
+  );
+};
