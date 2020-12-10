eff681c8cb525b0e845db062dd4978ed901b1101
Leif <leifl@kth.se>
2020-12-10 14:29:13 +0100

    specify db port in test

--- server/__tests__/integration/ChatDAOPostgres.test.js
+++ server/__tests__/integration/ChatDAOPostgres.test.js
@@ -49,9 +49,10 @@ const connectToChatDb = () => {
   }
   return pgp({
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    port: 5432,
     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
   });
 };
 
