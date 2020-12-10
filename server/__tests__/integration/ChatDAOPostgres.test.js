38290089cc94d9b1479d9f3be71eab329da8db83
Leif <leifl@kth.se>
2020-12-10 14:37:44 +0100

    debugging db test

--- server/__tests__/integration/ChatDAOPostgres.test.js
+++ server/__tests__/integration/ChatDAOPostgres.test.js
@@ -47,13 +47,14 @@ const connectToChatDb = () => {
   if (result.error) {
     throw result.error;
   }
-  return pgp({
+  const db = pgp({
     host: process.env.DB_HOST,
-    port: 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS
   });
+  console.log(db);
+  return db;
 };
 
 const clearDb = async () => {
